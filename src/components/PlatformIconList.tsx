dc29c466598ea07e976d15148d9eec7dfa809166
Kristjan Lääne <xonproject@gmail.com>
2024-05-31 22:00:46 +0300

    started to render platforms

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,9 @@
+import { Platform } from '../hooks/useGames';
+
+interface Props {
+  platforms: Platform[];
+}
+
+export const PlatformIconList = ({ platforms }: Props) => {
+  return <div>PlatformIconList</div>;
+};
