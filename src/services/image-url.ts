7a95e7b8c27444281e65dd2c9dfd3a2730186fb5
Kristjan Lääne <xonproject@gmail.com>
2024-06-08 15:24:34 +0300

    sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 export const getCroppedImageUrl = (url: string) => {
+  if (!url) return '';
   const taret = 'media/';
   const index = url.indexOf(taret) + taret.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
